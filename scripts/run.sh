c09d04fc0deacbb638fa5feddffc66a90ed089a7
Juanito Ortega <ortegaj83@gmail.com>
2022-10-20 10:13:42 -0400

    deploy files

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,7 @@ ls -la /vol/web
 whoami
 
 python manage.py wait_for_db
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
